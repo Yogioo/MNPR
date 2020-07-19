871fa779dc3fec593c8104a025a2ac740b8e2b14
Masaka <1273750265@qq.com>
2020-07-19 12:02:48 +0800

    Init

---
+++ Assets/Scripts/Editor/NPRStandardShaderGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MNPR.MNPREditor
+{
+    /// <summary>
+    /// NPR Shader GUI
+    /// </summary>
+    internal class NPRStandardShaderGUI : StandardShaderGUIBase
+    {
+        #region Property
+
+        protected MaterialProperty m_cullMode;
+
+        private static class CustomStyles
+        {
+            public static GUIContent CullMode = new GUIContent("Cull Mode");
+        }
+
+        #endregion 
+
+        public override void FindPropertiesEx(MaterialProperty[] props)
+        {
+            base.FindPropertiesEx(props);
+            m_cullMode = FindProperty("_CullMode", props);
+        }
+        public override void DoExtention()
+        {
+            base.DoExtention();
+            m_MaterialEditor.ShaderProperty(m_cullMode, CustomStyles.CullMode.text);
+        }
+    }
+}
